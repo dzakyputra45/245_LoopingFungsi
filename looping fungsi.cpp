067f2b14dcf389a3226ea8217eef6b0ac98bd390
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-25 12:48:48 +0700

    Create Looping Using Do While

--- looping fungsi.cpp
+++ looping fungsi.cpp
@@ -48,6 +48,25 @@ int main() {
     cout << "Bilangan" << bilangan << "adalah Genap" << endl;
 }
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string pilihan;
+    do
+    {
+        cout << "makan" << endl;
+        cout << "main game" << endl;
+        cout << "Ibadah" << endl;
+
+        cout << "Apakah anda masih hidup? (Yes/No)";
+        cin >> pilihan;
+
+    } while (pilihan == "Yes");
+
+}
+
 
 
 
